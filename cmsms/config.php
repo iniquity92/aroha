771d01906a10a07a9b987a4674fd6e0a93565e68
iniquity92 <sonu.abhisheksingh143@gmail.com>
2016-10-03 03:47:13 +0530

    Add files via upload

---
+++ cmsms/config.php
@@ -0,0 +1,14 @@
+<?php
+# CMS Made Simple Configuration File
+# Documentation: /doc/CMSMS_config_reference.pdf
+#
+$config['dbms'] = 'mysqli';
+$config['db_hostname'] = 'localhost';
+$config['db_username'] = 'root';
+$config['db_password'] = '';
+$config['db_name'] = 'cmsmsdb';
+$config['db_prefix'] = 'cms_';
+$config['timezone'] = 'Asia/Dili';
+$config['set_db_timezone'] = 1;
+$config['url_rewriting'] = 'mod_rewrite';
+?>
